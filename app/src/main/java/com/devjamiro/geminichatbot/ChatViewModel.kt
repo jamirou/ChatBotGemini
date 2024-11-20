f99f18cb1f320330b376d794923712e8e3ad691c
Jamiro <devjamiro@gmail.com>
2024-11-20 12:25:21 -0300

    Update ChatViewModel: Implement messageList state and integrate message handling

--- app/src/main/java/com/devjamiro/geminichatbot/ChatViewModel.kt
+++ app/src/main/java/com/devjamiro/geminichatbot/ChatViewModel.kt
@@ -1,12 +1,17 @@
 package com.devjamiro.geminichatbot
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.ai.client.generativeai.GenerativeModel
 import kotlinx.coroutines.launch
 
 class ChatViewModel : ViewModel() {
+
+    val messageList by lazy {
+        mutableStateListOf<MessageModel>()
+    }
 
     val generativeModel: GenerativeModel = GenerativeModel(
         modelName = "gemini-pro",
@@ -16,8 +21,11 @@ class ChatViewModel : ViewModel() {
     fun sendMessage(question: String) {
         viewModelScope.launch {
             val chat = generativeModel.startChat()
+
+            messageList.add(MessageModel(question, "user"))
+
             val response = chat.sendMessage(question)
-            Log.i("ChatViewModel", "Response: ${response.text.toString()}")
+            messageList.add(MessageModel(response.text.toString(), "model"))
         }
     }
 }
