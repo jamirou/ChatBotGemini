18fee0d634b032954a6c55f422284d243c9583ca
Jamiro <devjamiro@gmail.com>
2024-11-20 12:25:51 -0300

    Implement message list and row UI in ChatPage with LazyColumn

--- app/src/main/java/com/devjamiro/geminichatbot/ChatPage.kt
+++ app/src/main/java/com/devjamiro/geminichatbot/ChatPage.kt
@@ -1,11 +1,15 @@
 package com.devjamiro.geminichatbot
 
+import android.graphics.Paint.Align
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
@@ -19,20 +23,66 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.devjamiro.geminichatbot.ui.theme.ModelMessage
+import com.devjamiro.geminichatbot.ui.theme.UserMessage
 
 @Composable
 fun ChatPage(modifier: Modifier, viewModel: ChatViewModel) {
     Column(modifier = modifier) {
         AppHeader()
+        MessageList(modifier = Modifier.weight(1f), messageList = viewModel.messageList)
         MessageInput(
             onMessageSend = {
                 viewModel.sendMessage(it)
             }
         )
+    }
+}
+
+@Composable
+fun MessageList(modifier: Modifier = Modifier, messageList: List<MessageModel>) {
+    LazyColumn(
+        modifier = modifier,
+        reverseLayout = true
+    ) {
+        items(messageList.reversed()) {
+            MessageRow(messageModel = it)
+        }
+    }
+}
+
+@Composable
+fun MessageRow(messageModel: MessageModel) {
+    val isModel = messageModel.role == "model"
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .align(
+                        if (isModel) Alignment.BottomStart else Alignment.BottomEnd
+                    )
+                    .padding(
+                        start = if (isModel) 8.dp else 70.dp,
+                        end = if (isModel) 70.dp else 8.dp,
+                        top = 8.dp,
+                        bottom = 8.dp
+                    )
+                    .clip(RoundedCornerShape(48f))
+                    .background(if (isModel) ModelMessage else UserMessage)
+                    .padding(16.dp)
+            ) {
+                Text(text = messageModel.message, fontWeight = FontWeight.W500, color = Color.Black)
+            }
+        }
     }
 }
 
